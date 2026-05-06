c347d02c8f57697ff716ff477dc963791bbfc2f8
zjm <935406810@qq.com>
2026-05-06 14:48:39 +0800

    v0.4 环形阵列；事件

--- Assets/Scripts/MonoBehaviour/UnitSelectionManager.cs
+++ Assets/Scripts/MonoBehaviour/UnitSelectionManager.cs
@@ -3,8 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
 using Unity.Transforms;
 using UnityEngine;
+using Ray = UnityEngine.Ray;
 
 public class UnitSelectionManager : MonoBehaviour
 {
@@ -38,10 +41,17 @@ public class UnitSelectionManager : MonoBehaviour
 
             #region 取消选择所有单位
             EntityQuery entityQuery = new EntityQueryBuilder(Allocator.Temp).WithAll<Selected>().Build(entityManager);
+
             NativeArray<Entity> entityArray = entityQuery.ToEntityArray(Allocator.Temp);
+            NativeArray<Selected> selectedArray = entityQuery.ToComponentDataArray<Selected>(Allocator.Temp);
+
             for (int i = 0; i < entityArray.Length; i++)
             {
                 entityManager.SetComponentEnabled<Selected>(entityArray[i], false);
+
+                Selected selected = selectedArray[i];
+                selected.onDeselected = true;
+                entityManager.SetComponentData<Selected>(entityArray[i], selected);
             }
             #endregion
 
@@ -55,16 +65,65 @@ public class UnitSelectionManager : MonoBehaviour
             NativeArray<LocalTransform> localTransformArray = entityQuery.ToComponentDataArray<LocalTransform>(Allocator.Temp);
 
             Rect selectionAreaRect = GetSelectionAreaRect();
+            //选择区域大小, 判断是否进行框选多个单位
+            float selectionAreaSize = selectionAreaRect.width + selectionAreaRect.height;
+            float multipleSelectionSizeMin = 40f;
+            bool isMultipleSelection = selectionAreaSize > multipleSelectionSizeMin;
 
-            for (int i = 0; i < localTransformArray.Length; i++)
+            if (isMultipleSelection)
             {
-                LocalTransform unitLocalTransform = localTransformArray[i];
-                //获取单位屏幕位置
-                Vector2 unitScreenPosition = Camera.main.WorldToScreenPoint(unitLocalTransform.Position);
-                //判断是否在选择区
-                if (selectionAreaRect.Contains(unitScreenPosition))
+                for (int i = 0; i < localTransformArray.Length; i++)
                 {
-                    entityManager.SetComponentEnabled<Selected>(entityArray[i], true);
+                    LocalTransform unitLocalTransform = localTransformArray[i];
+                    //获取单位屏幕位置
+                    Vector2 unitScreenPosition = Camera.main.WorldToScreenPoint(unitLocalTransform.Position);
+                    //判断是否在选择区
+                    if (selectionAreaRect.Contains(unitScreenPosition))
+                    {
+                        entityManager.SetComponentEnabled<Selected>(entityArray[i], true);
+
+                        Selected selected = entityManager.GetComponentData<Selected>(entityArray[i]);
+                        selected.onSelected = true;
+                        entityManager.SetComponentData<Selected>(entityArray[i], selected);
+                    }
+                }
+            }
+            else
+            {
+                //单选
+                //等价查询
+                //new EntityQueryBuilder(Allocator.Temp).WithAll<PhysicsWorldSingleton>().Build(entityManager);
+                entityQuery = entityManager.CreateEntityQuery(typeof(PhysicsWorldSingleton));
+                //获取单例
+                PhysicsWorldSingleton physicsWorldSingleton = entityQuery.GetSingleton<PhysicsWorldSingleton>();
+
+                CollisionWorld collisionWorld = physicsWorldSingleton.CollisionWorld;
+
+                Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+                int unitsLayer = 6;
+                RaycastInput raycastInput = new RaycastInput()
+                {
+                    Start = cameraRay.GetPoint(0f),
+                    End = cameraRay.GetPoint(9999f),
+                    Filter = new CollisionFilter()
+                    {
+                        BelongsTo = ~0u,
+                        CollidesWith = 1u << unitsLayer,//Layer:Units 位掩码
+                        GroupIndex = 0
+                    }
+                };
+
+                if (collisionWorld.CastRay(raycastInput, out Unity.Physics.RaycastHit raycastHit))
+                {
+                    if(entityManager.HasComponent<Unit>(raycastHit.Entity))
+                    {
+                        //hit a unit
+                        entityManager.SetComponentEnabled<Selected>(raycastHit.Entity, true);
+
+                        Selected selected = entityManager.GetComponentData<Selected>(raycastHit.Entity);
+                        selected.onSelected = true;
+                        entityManager.SetComponentData<Selected>(raycastHit.Entity, selected);
+                    }
                 }
             }
             #endregion
@@ -93,11 +152,13 @@ public class UnitSelectionManager : MonoBehaviour
             //    entityManager.SetComponentData(entityArray[i], unitMover);
             //}
 
+            NativeArray<float3> movePositionArray = GenerateMovePositionArray(mouseWorldPosition, entityArray.Length);
+
             for (int i = 0; i < unitMoverArray.Length; i++)
             {
                 //副本 未修改存储在实体中的数据
                 UnitMover unitMover = unitMoverArray[i];
-                unitMover.targetPosition = mouseWorldPosition;
+                unitMover.targetPosition = /*mouseWorldPosition*/movePositionArray[i];
                 unitMoverArray[i] = unitMover;
             }
             entityQuery.CopyFromComponentDataArray(unitMoverArray);
@@ -115,9 +176,52 @@ public class UnitSelectionManager : MonoBehaviour
             Mathf.Max(selectionStartMousePosition.y, selectionEndMousePosition.y));
 
         return new Rect(
-            lowerLeftCorner.x, 
-            lowerLeftCorner.y, 
-            upperRightCorner.x - lowerLeftCorner.x, 
+            lowerLeftCorner.x,
+            lowerLeftCorner.y,
+            upperRightCorner.x - lowerLeftCorner.x,
             upperRightCorner.y - lowerLeftCorner.y);
+    }
+
+    public NativeArray<float3> GenerateMovePositionArray(float3 targetPosition, int positionCount)
+    {
+        NativeArray<float3> positionArray = new NativeArray<float3>(positionCount, Allocator.Temp);
+        if (positionCount == 0)
+        {
+            return positionArray;
+        }
+
+        positionArray[0] = targetPosition;
+        if (positionCount == 1)
+        {
+            return positionArray;
+        }
+
+        float ringSize = 2.2f;
+        int ring = 0;
+        int positionIndex = 1;
+
+        while (positionIndex < positionCount)
+        {
+            int ringPositionCount = 3 + ring * 2;
+
+            for(int i = 0; i < ringPositionCount; i++)
+            {
+                float angle = math.PI2 / ringPositionCount;
+                float3 ringVector = math.rotate(quaternion.RotateY(angle), new float3(ringSize * (ring + 1), 0, 0));
+                float3 ringPosition = targetPosition + ringVector;
+
+                positionArray[positionIndex] = ringPosition;
+                positionIndex++;
+
+                if (positionIndex >= positionCount)
+                {
+                    break;
+                }
+            }
+
+            ring++;
+        }
+
+        return positionArray;
     }
 }
--- Assets/Scripts/System/SelectedVisualSystem.cs
+++ Assets/Scripts/System/SelectedVisualSystem.cs
@@ -2,21 +2,40 @@ using Unity.Burst;
 using Unity.Entities;
 using Unity.Transforms;
 
+[UpdateInGroup(typeof(LateSimulationSystemGroup))]
+[UpdateBefore(typeof(ResetEventsSystem))]
 partial struct SelectedVisualSystem : ISystem
 {
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach (RefRO<Selected> selected in SystemAPI.Query<RefRO<Selected>>().WithDisabled<Selected>())
-        {
-            RefRW<LocalTransform> visualLocalTransform = SystemAPI.GetComponentRW<LocalTransform>(selected.ValueRO.visualEntity);
-            visualLocalTransform.ValueRW.Scale = 0;
-        }
+        //foreach (RefRO<Selected> selected in SystemAPI.Query<RefRO<Selected>>().WithDisabled<Selected>())
+        //{
+        //    RefRW<LocalTransform> visualLocalTransform = SystemAPI.GetComponentRW<LocalTransform>(selected.ValueRO.visualEntity);
+        //    visualLocalTransform.ValueRW.Scale = 0;
+        //}
 
-        foreach (RefRO<Selected> selected in SystemAPI.Query<RefRO<Selected>>())
+        //foreach (RefRO<Selected> selected in SystemAPI.Query<RefRO<Selected>>())
+        //{
+        //    RefRW<LocalTransform> visualLocalTransform = SystemAPI.GetComponentRW<LocalTransform>(selected.ValueRO.visualEntity);
+        //    visualLocalTransform.ValueRW.Scale = selected.ValueRO.showScale;
+        //}
+
+        #region 仅在状态改变时更新
+        foreach (RefRO<Selected> selected in SystemAPI.Query<RefRO<Selected>>().WithPresent<Selected>())
         {
-            RefRW<LocalTransform> visualLocalTransform = SystemAPI.GetComponentRW<LocalTransform>(selected.ValueRO.visualEntity);
-            visualLocalTransform.ValueRW.Scale = selected.ValueRO.showScale;
+            if (selected.ValueRO.onSelected)
+            {              
+                RefRW<LocalTransform> visualLocalTransform = SystemAPI.GetComponentRW<LocalTransform>(selected.ValueRO.visualEntity);
+                visualLocalTransform.ValueRW.Scale = selected.ValueRO.showScale;
+            }
+
+            if (selected.ValueRO.onDeselected)
+            {
+                RefRW<LocalTransform> visualLocalTransform = SystemAPI.GetComponentRW<LocalTransform>(selected.ValueRO.visualEntity);
+                visualLocalTransform.ValueRW.Scale = 0;
+            }
         }
+        #endregion
     }
 }
